da728ad64352b23720506d0c29e47690565a2bf8
Obafemi Lawal <swissbobo@gmail.com>
2022-10-10 22:05:31 +0300

    rest

--- es-77/exercise.js
+++ es-77/exercise.js
@@ -2,16 +2,23 @@ const isLogged = true;
 
 function firstPromise(logCheck) {
   return new Promise((resolve, reject) => {
-    if (isLogged) {
+    if (logCheck) {
       resolve(Math.floor(Math.random() * 9 + 1));
+    } else {
+      reject(new Error('something went wrong from first promise'));
     }
   });
 }
 
 function secondPromise(num) {
-  new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (typeof num === 'number' && num > 0.5) {
       resolve({ name: 'John', age: 24 });
-    } else reject(new Error('some error occured'));
+    } else reject(new Error('some error occured from second promise'));
   });
 }
+
+firstPromise(isLogged)
+  .then(secondPromise)
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
